f0c221765ab93da46712f5b7ee40e7a321f71ce9
Nels Romerdahl <7355705+NelsWebDev@users.noreply.github.com>
2025-05-05 03:03:14 -0700

    fix catching error onStartGame

--- backend/src/events/GamePlayEvents.ts
+++ backend/src/events/GamePlayEvents.ts
@@ -1,30 +1,45 @@
 import { game } from "../singletons";
 import { EventHandlers, RoundStatus, Rules, WhiteCard } from "../types";
 
-export const GamePlayEventHandlers: Pick<EventHandlers, 
-    "onPlayCards" | "onPickWinner" | "onStartGame" | "onEndGame" | "onGetGame" | "onMyHand" | "onSkipBlackCard" | "onVoteToSkipBlackCard" | "onUndoPlay"> = {
-
-    onPlayCards: function (socket, arg: WhiteCard[]): void {
-       socket.data.playWhiteCards(arg);
-    },
-    onUndoPlay: function (socket): void {
-        socket.data.undoPlay();
-    },
-    onPickWinner: function (socket, winnerId): void {
-        socket.data.selectWinner(winnerId);
-    },
-    onStartGame: function (): void {
-        game.start();
-    },
-    onEndGame: function (): void {
-        game.endGame();
-    },
-    onGetGame: function (socket): void {
-        socket.data.room.emit("game", game.toJSON());
-    },
-    onMyHand: function (socket): void {
-       socket.data.emitMyHand();
-    },
+export const GamePlayEventHandlers: Pick<
+  EventHandlers,
+  | "onPlayCards"
+  | "onPickWinner"
+  | "onStartGame"
+  | "onEndGame"
+  | "onGetGame"
+  | "onMyHand"
+  | "onSkipBlackCard"
+  | "onVoteToSkipBlackCard"
+  | "onUndoPlay"
+> = {
+  onPlayCards: function (socket, arg: WhiteCard[]): void {
+    socket.data.playWhiteCards(arg);
+  },
+  onUndoPlay: function (socket): void {
+    socket.data.undoPlay();
+  },
+  onPickWinner: function (socket, winnerId): void {
+    socket.data.selectWinner(winnerId);
+  },
+  onStartGame: function (socket): void {
+    game.start().catch((err) => {
+      console.error("Error starting game", err);
+      socket.emit("serverMessage", {
+        title: "Failed to start game",
+        message: err?.message,
+      });
+    });
+  },
+  onEndGame: function (): void {
+    game.endGame();
+  },
+  onGetGame: function (socket): void {
+    socket.data.room.emit("game", game.toJSON());
+  },
+  onMyHand: function (socket): void {
+    socket.data.emitMyHand();
+  },
 
     onSkipBlackCard: function (socket): void {
         if(game.currentCardCzar.id !== socket.data.id) {
